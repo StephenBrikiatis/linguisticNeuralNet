22901bd9ce8264b77edde4333a743bcd6b45dfcc
StephenBrikiatis <stephen.brikiatis@mymail.champlain.edu>
2017-04-13 22:59:18 -0400

    Update to removal of items from the tree, partially working

--- latticeCleaner.py
+++ latticeCleaner.py
@@ -25,6 +25,7 @@ treeArray = [[word,  0,  0]]
 #This for loop builds the tree by checking each item of the TreeArray in the current loop, and finding what they are connected to.
 rowSizes = []
 for rows in range(0,  4):
+    print(rows)
     holder = []
     arrayLength = len(treeArray);
     itemNumber = 0;
@@ -35,30 +36,21 @@ for rows in range(0,  4):
                 itemNumber += 1
 #this halve of the for loop removes extras and copies of words that would create a feedback loop
     rowSizes.append(itemNumber)
+    print(rowSizes)
+    print(treeArray)
     for identifierRow in range(0,  rows+1):
         total = 1
         for adder in rowSizes:
             total += adder
         subtotal = total - rowSizes[-1]
-        print(rowSizes)
-        print(treeArray)
         for identiferPositionX in range(0,  subtotal):
-            for identifierPositionY in range(0,  total):
-                print ("%02d, %02d" % (identiferPositionX,  identifierPositionY))
-                print(treeArray[identifierPositionY])
+            itemsToRemove = []
+            for identifierPositionY in range(0,  rowSizes[-1]):
+                identifierPositionY += subtotal
                 if(treeArray[identiferPositionX][0] == treeArray[identifierPositionY][0]):
-                    del treeArray[identiferPositionX]
-                    rowSizes[-1] -= 1
+                    itemsToRemove.append(identifierPositionY)
+            for removalItem in reversed(itemsToRemove):
+                del treeArray[removalItem]
+                rowSizes[-1] -= 1
                     
 print(treeArray)
-#holderItem = fixedLattice.get(word)
-#holderArray.append(holderItem)
-#holderArray.append(currentRow)
-#theRealLattice.append(holderArray)
-#
-#for times in range(0,  4):
-#    for item in holderItem:
-#        holderItem = fixedLattice.get(item, "empty")
-#        holderArray.append(holderItem)
-#    currentRow += 1
-#    theRealLattice.append(holderArray)    
